9cd022f71794e6caec006c11691a87d7a9981855
achilles822 <achilles822@icloud.com>
2021-03-11 17:21:24 +0800

    add:leetcode 152

---
+++ 删除一个字符是否回文-680.js
@@ -0,0 +1,64 @@
+// 描述：给定一个非空字符串 s，最多删除一个字符。判断是否能成为回文字符串。
+// 输入: "aba"
+// 输出: True
+// 示例 2:
+// 输入: "abca"
+// 输出: True
+// 解释: 你可以删除c字符。
+// 注意: 字符串只包含从 a-z 的小写字母。字符串的最大长度是50000。
+
+// 本题考察回文的对称性，可用双指针解题
+// const validPalindrome = function (s) {
+//   let i = 0,
+//     j = s.length - 1
+//   while (i < j && s[i] == s[j]) {
+//     i++
+//     j--
+//   }
+//   //   左侧指针跳过一个字符
+//   if (isPalindrome(i + 1, j)) {
+//     return true
+//   }
+//   //   右侧指针跳过一个字符
+//   if (isPalindrome(i, j - 1)) {
+//     return true
+//   }
+//   //   回文判断函数
+//   function isPalindrome (front, end) {
+//     while (front < end) {
+//       if (s[front] !== s[end]) {
+//         return false
+//       }
+//       front++
+//       end--
+//     }
+//     return true
+//   }
+
+//   return false
+// }
+
+const validPalindrome = function (s) {
+  let [i, j] = [0, s.length - 1]
+  while (i < j) {
+    if (s[i] !== s[j]) {
+      return isValid(s, i + 1, j) || isValid(s, i, j - 1)
+    }
+    i++
+    j--
+  }
+  return true
+}
+const isValid = function (str, l, r) {
+  while (l < r) {
+    if (str[l] !== str[r]) {
+      return false
+    }
+    l++
+    r--
+  }
+  return true
+}
+validPalindrome('abca') // true
+validPalindrome('aba') // true
+validPalindrome('abadc') // false
